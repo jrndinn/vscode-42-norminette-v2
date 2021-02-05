04998d525099f2299b884240b374253a6a60e6eb
underdisk <ajendu003@gmail.com>
2021-02-05 20:14:14 +0100

    added option for norminette path

--- out/extension.js
+++ out/extension.js
@@ -6,11 +6,18 @@ const vscode_1 = require("vscode");
 let diagnosticsCollection = vscode_1.languages.createDiagnosticCollection("NorminetteV2");
 function activate(context) {
     vscode.workspace.onDidSaveTextDocument((e) => {
+        let params = vscode.workspace.getConfiguration('norminettev2');
+        let bin = params.get("norminette_bin");
         if (e.languageId === "c" && e.uri.scheme === "file") {
             const cp = require('child_process');
             let str = "";
             str = e.uri.path;
-            cp.exec('norminettev2 ' + str + " | tail -n +2", (err, stdout, stderr) => {
+            cp.exec('which ' + bin, (err, stdout, stderr) => {
+                if (err) {
+                    vscode.window.showInformationMessage("Error: " + bin + " was not found");
+                }
+            });
+            cp.exec(bin + ' ' + str + " | tail -n +2", (err, stdout, stderr) => {
                 let parseLine = (line) => {
                     line = line.substr(30);
                     var ret = { line: 0, col: 0, desc: "No error" };
@@ -27,14 +34,14 @@ function activate(context) {
                     var r1 = e.lineAt(obj.line - 1);
                     var r2 = e.lineAt(obj.line - 1);
                     var r = new vscode.Range(r1.range.start, r2.range.end);
-                    diagnostics.push(new vscode_1.Diagnostic(r, obj.desc, vscode_1.DiagnosticSeverity.Error));
+                    diagnostics.push(new vscode_1.Diagnostic(r, obj.desc, vscode_1.DiagnosticSeverity.Warning));
                     diagnosticsCollection.set(e.uri, diagnostics);
                 }
             });
         }
     });
     let disposable = vscode.commands.registerCommand('42-norminette-v2.launchNorm', () => {
-        vscode.window.showInformationMessage("" + vscode.workspace.name);
+        vscode.window.showInformationMessage("unimplemented yet");
     });
     context.subscriptions.push(disposable);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,19 @@ let diagnosticsCollection = languages.createDiagnosticCollection("NorminetteV2")
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.workspace.onDidSaveTextDocument((e) => {
+
+		let params = vscode.workspace.getConfiguration('norminettev2');
+		let bin = params.get("norminette_bin");
 		if (e.languageId === "c" && e.uri.scheme === "file") {
 			const cp = require('child_process');
 			let str: string = "";
 			str = e.uri.path;
-			cp.exec('norminettev2 ' + str + " | tail -n +2", (err: string, stdout: string, stderr: string) => {
+			cp.exec('which ' + bin, (err: string, stdout: string, stderr: string) => {
+				if (err) {
+					vscode.window.showInformationMessage("Error: " + bin + " was not found");
+				}
+			});
+			cp.exec(bin + ' ' + str + " | tail -n +2", (err: string, stdout: string, stderr: string) => {
 				let parseLine = (line: string) => {
 					line = line.substr(30);
 					var ret = {line: 0, col: 0, desc: "No error"};
@@ -17,7 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
 					ret.col = +(line.split(":")[1].slice(0, -1));
 					ret.desc = line.split(":")[2].trim();
 					return ret;
-				}
+				};
 				
 				let lines = stdout.split("\n");
 				let diagnostics : Diagnostic[] = [];
@@ -28,7 +36,7 @@ export function activate(context: vscode.ExtensionContext) {
 					var r1 = e.lineAt(obj.line - 1);
 					var r2 = e.lineAt(obj.line - 1);
 					var r = new vscode.Range(r1.range.start, r2.range.end);
-					diagnostics.push(new Diagnostic(r, obj.desc, DiagnosticSeverity.Error));
+					diagnostics.push(new Diagnostic(r, obj.desc, DiagnosticSeverity.Warning));
 					diagnosticsCollection.set(e.uri, diagnostics);
 				}		
 			});
@@ -37,7 +45,7 @@ export function activate(context: vscode.ExtensionContext) {
 	
 
 	let disposable = vscode.commands.registerCommand('42-norminette-v2.launchNorm', () => {
-		vscode.window.showInformationMessage("" + vscode.workspace.name);
+		vscode.window.showInformationMessage("unimplemented yet");
 	});
 	
 
