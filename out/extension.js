4c7f66a27ac3b6e8cde7f1fae546f1950df05c9b
hroussea <hroussea@student.42lyon.fr>
2021-03-17 13:17:53 +0100

    Release!
    - Added possibility to launch norminette from the command palette
    - Added possibility to disable auto-launching the norminette of file
    save
    - Added possibility to clear the norminette decorators from the command
    palette
    - Changed default command to "norminette" instead of "norminettev2" to
    reflect changes on the 42 Lyon Campus
    - Renamed a few stuff to "Norminette v3" to avoid possible confusion

--- out/extension.js
+++ out/extension.js
@@ -5,9 +5,29 @@ const vscode = require("vscode");
 const vscode_1 = require("vscode");
 let diagnosticsCollection = vscode_1.languages.createDiagnosticCollection("NorminetteV2");
 function activate(context) {
-    vscode.workspace.onDidSaveTextDocument((e) => {
+    vscode.workspace.onDidSaveTextDocument((_e) => {
+        let params = vscode.workspace.getConfiguration('norminettev2');
+        if (params.get("auto_launch")) {
+            vscode.commands.executeCommand("42-norminette-v2.launchNorm");
+        }
+    });
+    let parseLine = (line) => {
+        var ret = { line: 0, col: 0, desc: "No error", id: "NO_ERROR" };
+        let rgx = /(\s*)(\w+)(\s*)(\()(line:\s*)([0-9]+)(,\s*)(col:\s*)([0-9]+)(\):)(\s*)(.+)/;
+        let match = rgx.exec(line);
+        if (match) {
+            ret.line = parseInt(match[6]);
+            ret.col = parseInt(match[9]);
+            ret.id = match[2];
+            ret.desc = match[12][0].toUpperCase() + match[12].slice(1);
+        }
+        return ret;
+    };
+    let _d0 = vscode.commands.registerCommand('42-norminette-v2.launchNorm', () => {
+        var _a;
         let params = vscode.workspace.getConfiguration('norminettev2');
         let bin = params.get("norminette_bin");
+        let e = (_a = vscode.window.activeTextEditor) === null || _a === void 0 ? void 0 : _a.document;
         if ((e.languageId === "c" || e.languageId === "cpp") && e.uri.scheme === "file") {
             const cp = require('child_process');
             let str = "";
@@ -18,36 +38,24 @@ function activate(context) {
                 }
             });
             cp.exec(bin + ' ' + str + " | tail -n +2", (err, stdout, stderr) => {
-                let parseLine = (line) => {
-                    var ret = { line: 0, col: 0, desc: "No error", id: "NO_ERROR" };
-                    let rgx = /(\s*)(\w+)(\s*)(\()(line:\s*)([0-9]+)(,\s*)(col:\s*)([0-9]+)(\):)(\s*)(.+)/;
-                    let match = rgx.exec(line);
-                    if (match) {
-                        ret.line = parseInt(match[6]);
-                        ret.col = parseInt(match[9]);
-                        ret.id = match[2];
-                        ret.desc = match[12][0].toUpperCase() + match[12].slice(1);
-                    }
-                    return ret;
-                };
                 let lines = stdout.split("\n");
                 let diagnostics = [];
                 diagnosticsCollection.clear();
                 for (let line in lines) {
                     var obj = parseLine(lines[line]);
-                    var r1 = e.lineAt(obj.line - 1);
-                    var r2 = e.lineAt(obj.line - 1);
-                    var r = new vscode.Range(r1.range.start, r2.range.end);
+                    var linePos = e.lineAt(obj.line - 1);
+                    var r = new vscode.Range(linePos.range.start, linePos.range.end);
                     diagnostics.push(new vscode_1.Diagnostic(r, obj.desc + " [" + obj.line + ":" + obj.col + ", " + obj.id + "]", vscode_1.DiagnosticSeverity.Warning));
                     diagnosticsCollection.set(e.uri, diagnostics);
                 }
             });
         }
     });
-    let disposable = vscode.commands.registerCommand('42-norminette-v2.launchNorm', () => {
-        vscode.window.showInformationMessage("unimplemented yet");
+    let _d1 = vscode.commands.registerCommand('42-norminette-v2.clear', () => {
+        diagnosticsCollection.clear();
     });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(_d0);
+    context.subscriptions.push(_d1);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,31 @@ import { languages, Diagnostic, DiagnosticSeverity } from 'vscode';
 let diagnosticsCollection = languages.createDiagnosticCollection("NorminetteV2");
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.workspace.onDidSaveTextDocument((e) => {
+	vscode.workspace.onDidSaveTextDocument((_e) => {
+		let params = vscode.workspace.getConfiguration('norminettev2');
+		if (params.get("auto_launch")) {
+				vscode.commands.executeCommand("42-norminette-v2.launchNorm");
+		}
+	});
 
+	let parseLine = (line: string) => {
+		var ret = {line: 0, col: 0, desc: "No error", id: "NO_ERROR"};
+		let rgx: RegExp = /(\s*)(\w+)(\s*)(\()(line:\s*)([0-9]+)(,\s*)(col:\s*)([0-9]+)(\):)(\s*)(.+)/;
+		let match = rgx.exec(line);
+		if (match) {
+			ret.line = parseInt(match[6]);
+			ret.col = parseInt(match[9]);
+			ret.id = match[2];
+			ret.desc = match[12][0].toUpperCase() + match[12].slice(1);
+		}
+		return ret;
+	};
+	
+
+	let _d0 = vscode.commands.registerCommand('42-norminette-v2.launchNorm', () => {
 		let params = vscode.workspace.getConfiguration('norminettev2');
 		let bin = params.get("norminette_bin");
+		let e = vscode.window.activeTextEditor?.document!;
 		if ((e.languageId === "c" ||  e.languageId === "cpp") && e.uri.scheme === "file") {
 			const cp = require('child_process');
 			let str: string = "";
@@ -18,42 +39,28 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			});
 			cp.exec(bin + ' ' + str + " | tail -n +2", (err: string, stdout: string, stderr: string) => {
-				let parseLine = (line: string) => {
-					var ret = {line: 0, col: 0, desc: "No error", id: "NO_ERROR"};
-					let rgx: RegExp = /(\s*)(\w+)(\s*)(\()(line:\s*)([0-9]+)(,\s*)(col:\s*)([0-9]+)(\):)(\s*)(.+)/;
-					let match = rgx.exec(line);
-					if (match) {
-						ret.line = parseInt(match[6]);
-						ret.col = parseInt(match[9]);
-						ret.id = match[2];
-						ret.desc = match[12][0].toUpperCase() + match[12].slice(1);
-					}
-					return ret;
-				};
-				
 				let lines = stdout.split("\n");
 				let diagnostics : Diagnostic[] = [];
 
 				diagnosticsCollection.clear();
 				for (let line in lines) {
 					var obj = parseLine(lines[line]);
-					var r1 = e.lineAt(obj.line - 1);
-					var r2 = e.lineAt(obj.line - 1);
-					var r = new vscode.Range(r1.range.start, r2.range.end);
+					var linePos = e.lineAt(obj.line - 1);
+					var r = new vscode.Range(linePos.range.start, linePos.range.end);
 					diagnostics.push(new Diagnostic(r, obj.desc + " [" + obj.line + ":" + obj.col + ", " + obj.id + "]", DiagnosticSeverity.Warning));
 					diagnosticsCollection.set(e.uri, diagnostics);
-				}		
+				}
 			});
 		}
 	});
-	
 
-	let disposable = vscode.commands.registerCommand('42-norminette-v2.launchNorm', () => {
-		vscode.window.showInformationMessage("unimplemented yet");
+	let _d1 = vscode.commands.registerCommand('42-norminette-v2.clear', () => {
+		diagnosticsCollection.clear();
 	});
 	
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(_d0);
+	context.subscriptions.push(_d1);
 }
 
 // this method is called when your extension is deactivated
